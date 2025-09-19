eebbaf05a6e4923a66ff95b75ed2ea624444c4b6
Sikboy79 <custombasewelds@yahoo.com>
2025-09-19 15:39:55 -0600

    Featured books

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
 import Nav from './components/Nav';
+import Featured from './components/Featured';
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }
